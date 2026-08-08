94173fd587f4b960f559e3630a1e7e05e6ce70b2
Saul Rodriguez <saulargz35@gmail.com>
2026-08-08 12:34:50 -0700

    changed LED pattern

--- l476_baremetal/Inc/app.h
+++ l476_baremetal/Inc/app.h
@@ -1,6 +1,14 @@
 #ifndef APP_H
 #define APP_H
 
+// data types
+
+typedef enum {
+    LED_STATE_ON,
+    LED_STATE_OFF
+} led_state_t;
+
+// function signatures
 void app_init(void);
 void app_update(void);
 
--- l476_baremetal/Src/app.c
+++ l476_baremetal/Src/app.c
@@ -5,19 +5,43 @@
 #include "timebase.h"
 
 #define LED_TOGGLE_PERIOD_MS 500u
+#define LED_ON_PERIOD_MS 100u
+#define LED_OFF_PERIOD_MS 900u
 
 static uint32_t last_toggle_ms;
+static led_state_t state;
 
 void app_init(){
     last_toggle_ms = timebase_get_ms();
     bsp_led_init();
+
+    led_state_t state;
+    state = LED_STATE_OFF;
 }
 
 void app_update(){
     const uint32_t now_ms = timebase_get_ms();
 
-    if((uint32_t)(now_ms - last_toggle_ms) >= LED_TOGGLE_PERIOD_MS){
-        last_toggle_ms += LED_TOGGLE_PERIOD_MS;
-        bsp_led_toggle();
+    switch (state){
+        case LED_STATE_OFF: {
+            
+            if((uint32_t)(now_ms - last_toggle_ms) >= LED_OFF_PERIOD_MS){ 
+                state = LED_STATE_ON;
+                last_toggle_ms += LED_OFF_PERIOD_MS;
+                bsp_led_on();
+            }
+            break; 
+        }
+
+        case LED_STATE_ON: {
+            if((uint32_t)(now_ms - last_toggle_ms) >= LED_ON_PERIOD_MS){
+                state = LED_STATE_OFF;
+                last_toggle_ms += LED_ON_PERIOD_MS;
+                bsp_led_off();
+            }
+            break;
+        }
     }
 }
+
+
